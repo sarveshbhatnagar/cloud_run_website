0fc32b1dc71793d11b3fa70c05d29196705075ed
Sarvesh Bhatnagar <sarveshbhatnagar08@gmail.com>
2022-01-18 19:33:37 -0500

    checking bug with --allow-unauthenticated flag

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello Somen {}!".format(name)
+    return "Hello there {}!".format(name)
 
 
 if __name__ == "__main__":
